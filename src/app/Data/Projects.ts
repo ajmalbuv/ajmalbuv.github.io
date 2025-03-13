aa01de6a5bdd3d9800e94a71ffdb4922b99499fa
Ajmal Basheer <77728850+ajmalbuv@users.noreply.github.com>
2025-03-13 22:22:45 +0530

    testing 3

--- src/app/Data/Projects.ts
+++ src/app/Data/Projects.ts
@@ -1,118 +1,57 @@
-export const Projects = [
+export const Skill = [
   {
-    id: 1,
-    img: 'assets/images/projects/images/ems.webp',
-    title: 'EduManage - College Management System',
-    description:
-      'EduManage is a web-based college management system built using Django and PostgreSQL. It streamlines administrative tasks such as student enrollment, timetable scheduling, attendance tracking, and performance evaluation.',
-    largeDescription:
-      'EduManage is a scalable and secure college management system designed to optimize academic administration. Built with Django and PostgreSQL, it offers features such as student enrollment, course and timetable management, attendance tracking, and performance assessment. The system supports multi-user access, allowing administrators, faculty, and students to interact seamlessly. EduManage is deployed on an Ubuntu VPS using Gunicorn and Certbot for SSL security, ensuring high availability and data integrity. Additionally, it features a serverless deployment on Vercel for optimized API performance. By implementing efficient database indexing and query optimization, EduManage achieves enhanced system performance and scalability.',
-    techstack:
-      'Python, Django, PostgreSQL, Docker, HTML, CSS, JavaScript, Bootstrap, Vercel, Ubuntu VPS, Gunicorn, Certbot.',
-    features: [
-      {
-        heading: 'User Roles & Authentication',
-        description:
-          'Secure multi-role authentication system allowing administrators, faculty, and students to access role-specific functionalities.',
-      },
-      {
-        heading: 'Student & Course Management',
-        description:
-          'Admins can enroll students, assign courses, and manage faculty allocations efficiently through an intuitive interface.',
-      },
-      {
-        heading: 'Attendance Tracking',
-        description:
-          'Automated attendance management system with real-time updates, allowing faculty to record attendance efficiently.',
-      },
-      {
-        heading: 'Performance Evaluation',
-        description:
-          'Integrated student assessment module for tracking academic progress and generating detailed performance reports.',
-      },
-      {
-        heading: 'Timetable Scheduling',
-        description:
-          'Customizable timetable generation for courses and faculty, ensuring efficient class scheduling and conflict resolution.',
-      },
-      {
-        heading: 'Secure Cloud Deployment',
-        description:
-          'Deployed on an Ubuntu VPS using Gunicorn for robust backend performance, with Certbot SSL encryption for secure data transactions.',
-      },
-      {
-        heading: 'Serverless API Deployment',
-        description:
-          'Integrated with Vercel for fast, scalable API performance, reducing server load and ensuring smooth data processing.',
-      },
+    type: 'Programming Languages',
+    skills: [
+      { name: 'Python', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg' },
+      { name: 'JavaScript', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg' },
+      { name: 'TypeScript', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg' },
+      { name: 'Java', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg' },
     ],
-    screenshots: {
-      img1: '/assets/images/projects/screenshots/EduManage/edumanage-1.webp',
-      img2: '/assets/images/projects/screenshots/EduManage/edumanage-2.webp',
-      img3: '/assets/images/projects/screenshots/EduManage/edumanage-3.webp',
-      img4: '/assets/images/projects/screenshots/EduManage/edumanage-4.webp',
-      img5: '/assets/images/projects/screenshots/EduManage/edumanage-5.webp',
-      img6: '/assets/images/projects/screenshots/EduManage/edumanage-6.webp',
-      img7: '/assets/images/projects/screenshots/EduManage/edumanage-7.webp',
-      img8: '/assets/images/projects/screenshots/EduManage/edumanage-8.webp',
-      img9: '/assets/images/projects/screenshots/EduManage/edumanage-9.webp',
-      img10: '/assets/images/projects/screenshots/EduManage/edumanage-10.webp',
-      img11: '/assets/images/projects/screenshots/EduManage/edumanage-11.webp',
-      img12: '/assets/images/projects/screenshots/EduManage/edumanage-12.webp',
-      img13: '/assets/images/projects/screenshots/EduManage/edumanage-13.webp',
-      img14: '/assets/images/projects/screenshots/EduManage/edumanage-14.webp',
-      img15: '/assets/images/projects/screenshots/EduManage/edumanage-15.webp',
-      img16: '/assets/images/projects/screenshots/EduManage/edumanage-16.webp',
-      img17: '/assets/images/projects/screenshots/EduManage/edumanage-17.webp',
-      img18: '/assets/images/projects/screenshots/EduManage/edumanage-18.webp',
-      img19: '/assets/images/projects/screenshots/EduManage/edumanage-19.webp',
-      img20: '/assets/images/projects/screenshots/EduManage/edumanage-20.webp',
-      img21: '/assets/images/projects/screenshots/EduManage/edumanage-21.webp',
-    },
   },
   {
-    id: 2,
-    img: 'assets/images/projects/images/portfolio.webp',
-    title: 'Personal Portfolio Website',
-    description:
-      'A personal portfolio website developed to showcase my projects, skills, and experiences. Built with Angular and Tailwind CSS, it offers a responsive and interactive user experience.',
-    largeDescription:
-      'This project is my personal portfolio website designed to highlight my professional journey, including projects, technical skills, and work experiences. Developed using Angular for a dynamic frontend and styled with Tailwind CSS for a sleek design, the website ensures compatibility across various devices and screen sizes. It features sections like About Me, Projects, Experience, and Contact, providing visitors with comprehensive information about my background and work. The site is hosted on GitHub Pages, ensuring high availability and ease of access.',
-    techstack: 'Angular, Tailwind CSS, JavaScript, HTML5, CSS3, GitHub Pages.',
-    features: [
-      {
-        heading: 'Responsive Design',
-        description:
-          'Ensures optimal viewing experience across a wide range of devices, from desktops to mobile phones.',
-      },
-      {
-        heading: 'Project Showcase',
-        description:
-          'Detailed presentations of selected projects with descriptions, technologies used, and live demos or source code links.',
-      },
-      {
-        heading: 'Interactive UI',
-        description:
-          'Engaging user interface with smooth navigation and interactive elements to enhance user experience.',
-      },
-      {
-        heading: 'Contact Form',
-        description:
-          'Integrated contact form allowing visitors to reach out directly through the website.',
-      },
-      {
-        heading: 'Deployment',
-        description:
-          'Hosted on GitHub Pages, providing a reliable and fast hosting solution with continuous deployment capabilities.',
-      },
+    type: 'Web Technologies',
+    skills: [
+      { name: 'HTML5', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg' },
+      { name: 'CSS3', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg' },
+      { name: 'Tailwind CSS', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg' },
     ],
-    screenshots: {
-      img1: '/assets/images/projects/screenshots/portfolio/portfolio-1.webp',
-      img2: '/assets/images/projects/screenshots/portfolio/portfolio-2.webp',
-      img3: '/assets/images/projects/screenshots/portfolio/portfolio-3.webp',
-      img4: '/assets/images/projects/screenshots/portfolio/portfolio-4.webp',
-      img5: '/assets/images/projects/screenshots/portfolio/portfolio-5.webp',
-      img6: '/assets/images/projects/screenshots/portfolio/portfolio-6.webp',
-    },
+  },
+  {
+    type: 'Database Management Systems',
+    skills: [
+      { name: 'PostgreSQL', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg' },
+      { name: 'MySQL', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg' },
+      { name: 'MongoDB', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg' },
+    ],
+  },
+  {
+    type: 'Frameworks and Libraries',
+    skills: [
+      { name: 'FastAPI', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/fastapi/fastapi-original.svg' },
+      { name: 'Express.js', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg' },
+      { name: 'Redux', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg' },
+      { name: 'JWT', image: '/assets/images/skills/web/jwt.svg' }, // Local path for JWT
+      { name: 'Redis', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redis/redis-original.svg' },
+      { name: 'RESTful APIs', image: '/assets/images/skills/web/rest.svg' }, // Local path for RESTful APIs
+      { name: 'Angular', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg' },
+      { name: 'Django', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg' },
+      { name: 'Flutter', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-original.svg' },
+      { name: 'Bootstrap', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg' },
+      { name: 'Vue.js', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg' }, // Added Vue.js as an example
+    ],
+  },
+  {
+    type: 'DevOps & Cloud Platforms',
+    skills: [
+      { name: 'Docker', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg' },
+      { name: 'Git', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg' },
+      { name: 'AWS', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-original-wordmark.svg' },
+      { name: 'Vercel', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vercel/vercel-original.svg' },
+      { name: 'Oracle Cloud', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/oracle/oracle-original.svg' },
+      { name: 'Firebase', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg' },
+      { name: 'Nginx', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nginx/nginx-original.svg' },
+      { name: 'GitLab', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/gitlab/gitlab-original.svg' }, // Added GitLab
+      { name: 'Kubernetes', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/kubernetes/kubernetes-plain.svg' }, // Added Kubernetes
+    ],
   },
 ];
