615c6e7fad9d643e910853fe87226e45097d7740
Ajmal Basheer <77728850+ajmalbuv@users.noreply.github.com>
2025-03-13 22:06:06 +0530

    Update Skill.ts

--- src/app/Data/Skill.ts
+++ src/app/Data/Skill.ts
@@ -2,239 +2,73 @@ export const Skill = [
   {
     type: 'Programming Languages',
     skills: [
-      {
-        name: 'Python',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg',
-      },
-      {
-        name: 'JavaScript',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg',
-      },
-      {
-        name: 'TypeScript',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg',
-      },
-      {
-        name: 'Dart',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dart/dart-original.svg',
-      },
-      {
-        name: 'Java',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg',
-      },
-      {
-        name: 'C',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg',
-      },
-      {
-        name: 'PHP',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg',
-      },
+      { name: 'Python', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg' },
+      { name: 'JavaScript', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg' },
+      { name: 'TypeScript', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg' },
+      { name: 'Dart', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/dart/dart-original.svg' },
+      { name: 'Java', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg' },
+      { name: 'C', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/c/c-original.svg' },
+      { name: 'PHP', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-original.svg' },
     ],
   },
   {
     type: 'Web Technologies',
     skills: [
-      {
-        name: 'HTML5',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg',
-      },
-      {
-        name: 'CSS3',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg',
-      },
-      {
-        name: 'Tailwind CSS',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg',
-      },
-      {
-        name: 'React',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg',
-      },
-      {
-        name: 'Angular',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg',
-      },
-      {
-        name: 'Node.js',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg',
-      },
-      {
-        name: 'Django',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg',
-      },
-      {
-        name: 'Flutter',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-original.svg',
-      },
-      {
-        name: 'Bootstrap',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg',
-      },
+      { name: 'HTML5', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg' },
+      { name: 'CSS3', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg' },
+      { name: 'Tailwind CSS', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-original.svg' },
+      { name: 'React', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg' },
+      { name: 'Angular', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/angularjs/angularjs-original.svg' },
+      { name: 'Node.js', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg' },
+      { name: 'Django', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg' },
+      { name: 'Flutter', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flutter/flutter-original.svg' },
+      { name: 'Bootstrap', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg' },
     ],
   },
   {
     type: 'Database Management Systems',
     skills: [
-      {
-        name: 'PostgreSQL',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg',
-      },
-      {
-        name: 'MySQL',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg',
-      },
-      {
-        name: 'MongoDB',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg',
-      },
-      {
-        name: 'SQLite',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-original.svg',
-      },
-      {
-        name: 'Firebase',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg',
-      },
+      { name: 'PostgreSQL', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg' },
+      { name: 'MySQL', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg' },
+      { name: 'MongoDB', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg' },
+      { name: 'SQLite', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlite/sqlite-original.svg' },
+      { name: 'Firebase', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg' },
     ],
   },
   {
     type: 'Frameworks and Libraries',
     skills: [
-      {
-        name: 'FastAPI',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/fastapi/fastapi-original.svg',
-      },
-      {
-        name: 'Express.js',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg',
-      },
-      {
-        name: 'Redux',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg',
-      },
-      {
-        name: 'JWT',
-        image: '/assets/images/skills/web/jwt.svg',
-      },
-      {
-        name: 'Redis',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redis/redis-original.svg',
-      },
-      {
-        name: 'RESTful APIs',
-        image: '/assets/images/skills/web/rest.svg',
-      },
+      { name: 'FastAPI', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/fastapi/fastapi-original.svg' },
+      { name: 'Express.js', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg' },
+      { name: 'Redux', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg' },
+      { name: 'JWT', image: '/assets/images/skills/web/jwt.svg' }, // Local path for JWT
+      { name: 'Redis', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redis/redis-original.svg' },
+      { name: 'RESTful APIs', image: '/assets/images/skills/web/rest.svg' }, // Local path for RESTful APIs
     ],
   },
   {
     type: 'DevOps & Cloud Platforms',
     skills: [
-      {
-        name: 'Docker',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg',
-      },
-      {
-        name: 'Git',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg',
-      },
-      {
-        name: 'AWS',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-original-wordmark.svg',
-      },
-      {
-        name: 'Vercel',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vercel/vercel-original.svg',
-      },
-      {
-        name: 'Oracle Cloud',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/oracle/oracle-original.svg',
-      },
-      {
-        name: 'Firebase',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg',
-      },
-      {
-        name: 'Nginx',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nginx/nginx-original.svg',
-      },
+      { name: 'Docker', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/docker/docker-original.svg' },
+      { name: 'Git', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg' },
+      { name: 'AWS', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-original-wordmark.svg' },
+      { name: 'Vercel', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vercel/vercel-original.svg' },
+      { name: 'Oracle Cloud', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/oracle/oracle-original.svg' },
+      { name: 'Firebase', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/firebase/firebase-plain.svg' },
+      { name: 'Nginx', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nginx/nginx-original.svg' },
     ],
   },
   {
     type: 'IDE’s & Other Tools',
     skills: [
-      {
-        name: 'VS Code',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg',
-      },
-      {
-        name: 'PyCharm',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pycharm/pycharm-original.svg',
-      },
-      {
-        name: 'IntelliJ IDEA',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/intellij/intellij-original.svg',
-      },
-      {
-        name: 'Eclipse',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/eclipse/eclipse-original.svg',
-      },
-      {
-        name: 'Postman',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postman/postman-original.svg',
-      },
-      {
-        name: 'Figma',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg',
-      },
-      {
-        name: 'LaTeX',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/latex/latex-original.svg',
-      },
-      {
-        name: 'Markdown',
-        image:
-          'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/markdown/markdown-original.svg',
-      },
+      { name: 'VS Code', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg' },
+      { name: 'PyCharm', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/pycharm/pycharm-original.svg' },
+      { name: 'IntelliJ IDEA', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/intellij/intellij-original.svg' },
+      { name: 'Eclipse', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/eclipse/eclipse-original.svg' },
+      { name: 'Postman', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postman/postman-original.svg' },
+      { name: 'Figma', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg' },
+      { name: 'LaTeX', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/latex/latex-original.svg' },
+      { name: 'Markdown', image: 'https://cdn.jsdelivr.net/gh/devicons/devicon/icons/markdown/markdown-original.svg' },
     ],
   },
 ];
