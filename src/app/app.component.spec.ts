d53db659a0b9b2935c652abb377c6193cb6fded8
Arch USB <ajmal@archusb.aju>
2025-07-27 14:44:43 +0530

    changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should have the \'Portfolio\' title', () => {
+  it("should have the 'Portfolio' title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Portfolio');
