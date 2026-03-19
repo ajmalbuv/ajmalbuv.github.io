8005a453fe2782976dde3f0935f8518617bb28b9
Ajmal Basheer <77728850+ajmalbuv@users.noreply.github.com>
2026-03-19 15:19:10 +0530

    nice nice nice

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://ajmalbuv.github.io",
+  site: "https://ajmalbuv.pages.dev",
   integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
