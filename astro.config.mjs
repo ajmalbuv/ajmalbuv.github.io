b3c0837e846c512a016751aea8a5c5e3e64c281f
Ajmal Basheer <77728850+ajmalbuv@users.noreply.github.com>
2026-03-22 23:32:53 +0530

    checking 456

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,9 @@ export default defineConfig({
   integrations: [icon()],
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      assetsInlineLimit: 0,
+    },
   },
   build: {
     assets: "assets",
