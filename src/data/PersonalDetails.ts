5419d4f29e39917f31628d23e19646aad25e78b3
Ajmal Basheer <77728850+ajmalbuv@users.noreply.github.com>
2026-03-26 13:53:54 +0530

    whoaaaaa

--- src/data/PersonalDetails.ts
+++ src/data/PersonalDetails.ts
@@ -1,34 +1,35 @@
-import type { PersonalDetails as PersonalDetailsType } from "../types/personalDetails.ts";
-import photo from "../assets/images/personal/photo.webp";
+import photo from '../assets/images/personal/photo.webp';
+import type { PersonalDetails as PersonalDetailsType } from '../types/personalDetails.ts';
 
 export const PersonalDetails: PersonalDetailsType = {
-  name: "Ajmal Basheer",
+  name: 'Ajmal Basheer',
   img: photo,
-  resume: "https://raw.githubusercontent.com/ajmalbuv/resume/master/resume-no-image.pdf",
-  designation: ["Flutter Mobile Developer", "Full Stack Developer"],
+  resume:
+    'https://media.githubusercontent.com/media/ajmalbuv/resume/refs/heads/master/resume-no-image.pdf',
+  designation: ['Flutter Mobile Developer', 'Full Stack Developer'],
   description:
-    "A results-driven software developer with expertise in both backend and frontend technologies. I build scalable, high-performance applications using modern frameworks and cloud services. Passionate about crafting seamless user experiences and robust, secure systems, I deliver end-to-end solutions for complex technical challenges.",
+    'A results-driven software developer with expertise in both backend and frontend technologies. I build scalable, high-performance applications using modern frameworks and cloud services. Passionate about crafting seamless user experiences and robust, secure systems, I deliver end-to-end solutions for complex technical challenges.',
   contact: {
     email: {
-      id: "ajmalbuv@gmail.com",
-      icon: "mdi:email",
+      id: 'ajmalbuv@gmail.com',
+      icon: 'mdi:email',
     },
     phone: {
-      number: "+91 9496444520",
-      icon: "mdi:phone",
+      number: '+91 9496444520',
+      icon: 'mdi:phone',
     },
-    address: "Bengaluru, Karnataka",
+    address: 'Bengaluru, Karnataka',
     github: {
-      url: "https://github.com/ajmalbuv",
-      icon: "mdi:github",
+      url: 'https://github.com/ajmalbuv',
+      icon: 'mdi:github',
     },
     linkedin: {
-      url: "https://www.linkedin.com/in/ajmalbuv/",
-      icon: "mdi:linkedin",
+      url: 'https://www.linkedin.com/in/ajmalbuv/',
+      icon: 'mdi:linkedin',
     },
     instagram: {
-      url: "https://www.instagram.com/_.aju",
-      icon: "mdi:instagram",
+      url: 'https://www.instagram.com/_.aju',
+      icon: 'mdi:instagram',
     },
   },
 };
